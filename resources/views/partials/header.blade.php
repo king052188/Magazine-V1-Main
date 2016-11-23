007d189480f2e8d747d1082885f0928537db2617
March <marchjigtala@yahoo.com>
2016-11-23 23:19:16 +0800

    Fix Logout

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -5,7 +5,7 @@
         </div>
         <ul class="nav navbar-top-links navbar-right">
             <li>
-                <a href="login.html">
+                <a href="{{ URL('/logout_process') }}">
                     <i class="fa fa-sign-out"></i> Log out
                 </a>
             </li>
