7df55cc19f1e0e8c17f899299a4ea5da422b30c9
Von Romson Bayani <von@cdgpacific.com>
2016-12-21 17:53:56 +0800

    Receiving layout. no functions yet.

--- resources/views/booking/booking_list.blade.php
+++ resources/views/booking/booking_list.blade.php
@@ -6,15 +6,16 @@
 
 @section('styles')
 <link href="{{ asset('css/plugins/dataTables/datatables.min.css') }}" rel="stylesheet">
+<link href="{{ asset('css/plugins/datepicker/datepicker3.css') }}" rel="stylesheet">
 @endsection
 
 @section('magazine_content')
 <div class="row wrapper border-bottom white-bg page-heading">
     <div class="col-lg-8">
-        <h2>Booking</h2>
+        <h2>Payment</h2>
         <ol class="breadcrumb">
             <li class="active">
-                <strong>Booking List</strong>
+                <strong>Receiving</strong>
             </li>
         </ol>
     </div>
@@ -26,11 +27,12 @@
 </div>
 
 <div class="wrapper wrapper-content animated fadeInRight">
+
     <div class="row">
         <div class="col-lg-12">
-        <div class="ibox float-e-margins">
+            <div class="ibox float-e-margins">
             <div class="ibox-title">
-                <h5>Booking List</h5>
+                <h5>Receiving</h5>
                 <div class = "pull-right">
                     <select class="form-control" id = "filter" style = "margin-top: -7px;">
                         <option disabled>--select--</option>
@@ -41,7 +43,7 @@
                         <option value = "5" {{ $filter == 5 ? "selected" : "" }}>Void</option>
                     </select>
                 </div>
-                <div style = "float: right; margin-right: 5px; font-size: 15px;"><label>Sort by:</label></div>
+                <div style = "float: right; margin-right: 5px; font-size: 15px;"><label>Sort by:</label></div>               
             </div>
 
             <div class="ibox-content">
@@ -53,114 +55,149 @@
                 </div>
                 @endif
 
-                <div class="table-responsive">
-                    <table id="tbl_booking_lists" class="table table-striped table-bordered table-hover dataTables-example" >
-                        <thead>
-                            <tr>
-                                <th style='text-align: center; width: 30px;'>#</th>
-                                <th style='text-align: left;'>Magazine Name</th>
-                                <th style='text-align: left; width: 50px;'># Issue</th>
-                                <th style='text-align: left; width: 50px;'>Year Issue</th>
-                                <th style='text-align: left; width: 150px;'>Sales Rep</th>
-                                <th style='text-align: left; width: 150px;'>Client</th>
-                                <th style='text-align: left; width: 150px;'>Agency</th>
-                                <th style='text-align: center; width: 50px;'>Line Item</th>
-                                <th style='text-align: center; width: 100px;'>Status / Action</th>
-                            </tr>
-                        </thead>
-                            <tbody>
-                                <?php
-                                    $n = 1;
-                                    $report_api = \App\Http\Controllers\AssemblyClass::get_reports_api();
-                                ?>
-                                @for($i = 0; $i < COUNT($booking); $i++)
-                                    <tr>
-                                        <td style='text-align: center;'>{{ $n++ }}</td>
-                                        <td style='text-align: left;'><a href = "{{ URL('/booking/magazine-transaction' . '/' . $booking[$i]->Id . '/' . $booking[$i]->magazine_country_id . '/' . $booking[$i]->client_id ) }}">{{ $booking[$i]->magazine_name  }}</a></td>
-                                        <td style='text-align: center;'>{{ $booking[$i]->magazine_issues }}</td>
-                                        <td style='text-align: center;'>{{ $booking[$i]->magazine_year }}</td>
-                                        <td style='text-align: left;'>{{ $booking[$i]->sales_rep_name }}</td>
-                                        <td style='text-align: left;'>{{ $booking[$i]->client_name }}</td>
-                                        <td style='text-align: left;'>{{ $booking[$i]->agency_name == null ? "NONE" : $booking[$i]->agency_name  }}</td>
-                                        <td style='text-align: center;'>{{ $booking[$i]->number_of_issue }}</td>
-                                    @if($_COOKIE['role'] > 2)
-                                        <td style='text-align: right; width: 280px;'>
-                                            <form class="form-inline">
-                                                <div class="form-group">
-                                                    <select style = "width: 150px;" class="form-control" id="ddlStatus_{{ $booking[$i]->Id }}" {{ ($booking[$i]->status == 5 OR $booking[$i]->status == 3)  ? "disabled" : "" }}>
-                                                        <optgroup label="-- Status --"> -- Status -- </optgroup>
-                                                        @if($booking[$i]->status == 5)
-                                                            <option value="0">Void</option>
-                                                        @elseif($booking[$i]->status == 3)
-                                                            <option value="0">Approved</option>
-                                                        @else
-                                                            <option {{ $booking[$i]->status == 1 ? "selected=true" : "" }} value="1">Pending</option>
-                                                            <option {{ $booking[$i]->status == 2 ? "selected=true" : "" }} value="2">For Approval</option>
-                                                            <optgroup label="-- Action --"> -- Action -- </optgroup>
-                                                                <option value = "-1:{{ $booking[$i]->trans_num  }}">Preview</option>
-                                                                <option value = "-2:{{ $booking[$i]->trans_num  }}">View As Client</option>
-                                                            </optgroup>
-                                                        @endif
-                                                    </select>
-                                                    @if($booking[$i]->status == 5)
-                                                        <a class="btn btn-primary" style = "width: 80px; margin-bottom: 0px;" onclick="update_status('{{ $booking[$i]->Id  }}','{{ $booking[$i]->trans_num  }}')" style="margin-bottom: 0;">Preview</a>
-                                                    @elseif($booking[$i]->status == 3)
-                                                        <a class="btn btn-primary" style = "width: 80px; margin-bottom: 0px;" onclick="update_status('{{ $booking[$i]->Id  }}','{{ $booking[$i]->trans_num  }}')" style="margin-bottom: 0;">Preview</a>
-                                                    @else
-                                                        <a class="btn btn-info" id="btn_update" style = "width: 80px; margin-bottom: 0px;" onclick="update_status('{{ $booking[$i]->Id  }}','{{ $booking[$i]->trans_num  }}')" style="margin-bottom: 0;">Update</a>
-                                                    @endif
-                                                   </div>
-                                            </form>
-                                        </td>
-                                    @else
-                                    <td style='text-align: center; width: 280px;'>
-                                        <div class="form-inline">
-                                            <div class="form-group">
-                                            <ul class="dropdown-menu" aria-labelledby="dropdownMenuDivider">
-                                              ...
-                                              <li role="separator" class="divider"></li>
-                                              ...
-                                            </ul>
-                                                <select style = "width: 150px;" class="form-control" id="ddlStatus_{{ $booking[$i]->Id }}">
-                                                    <optgroup label="-- Status --"> -- Status -- </optgroup>
-                                                    <option {{ $booking[$i]->status == 1 ? "selected=true" : "" }} value = "1">Pending</option>
-                                                    <option {{ $booking[$i]->status == 2 ? "selected=true" : "" }} value = "2">For Approval</option>
-                                                    <option {{ $booking[$i]->status == 3 ? "selected=true" : "" }} value = "3">Approved</option>
-                                                    {{--<option {{ $booking[$i]->status == 4 ? "selected=true" : "" }} value = "4">Declined</option>--}}
-                                                    <option {{ $booking[$i]->status == 5 ? "selected=true" : "" }} value = "5">Void</option>
-                                                    <optgroup label="-- Action --"> -- Action -- </optgroup>
-                                                    <option value = "-1:{{ $booking[$i]->trans_num  }}">Preview</option>
-                                                    <option value = "-2:{{ $booking[$i]->trans_num  }}">View As Client</option>
-                                                    </optgroup>
-                                                </select>
-                                                <button class="btn btn-primary" id="btn_update" style = "width: 80px;margin-bottom: 0px;" onclick="update_status('{{ $booking[$i]->Id  }}','{{ $booking[$i]->trans_num  }}')" style="margin-bottom: 0;">Update</button>
+
+                <div id="filter-panel" class="filter-panel collapse">
+                    <div class="panel panel-default">
+                        <div class="panel-body">
+                            <div class="col-sm-12">
+
+                                <form class="form-inline" role="form">
+                                    <div class="">
+                                        <div class="form-group" style="margin-right: 10px;">
+                                            <label class="filter-col" style="margin-right:0;" for="pref-search">Find by Invoice Number:</label><br/>
+                                            <input type="text" class="form-control" id="" placeholder="Input Invoice Number">
+                                        </div>
+                                        <div class="form-group">
+                                            <label class="filter-col" style="margin-right:0;" for="pref-search">&nbsp;</label><br/>
+                                            <button type="submit" class="btn btn-primary filter-col " style="margin-bottom: 0px;">
+                                                <i class="fa fa-searh"></i> Search
+                                            </button> 
+                                        </div>
+
+                                    </div><br/>
+
+                                    <div>
+                                        <div class="form-group" id="data_1">
+                                        <label class="filter-col" style="margin-right:0;" for="pref-perpage">Date of Payment:</label><br/>
+                                            <div class="input-group date">
+                                                <span class="input-group-addon"><i class="fa fa-calendar"></i></span><input type="text" class="form-control" value="03/04/2014">
                                             </div>
                                         </div>
-                                    </td>
-                                    @endif
-                                    </tr>
-                                    @endfor
+                                    </div><br/>
+
+
+                                    <div class="">
+                                        <div class="form-group" style="margin-right: 10px;">
+                                            <label class="filter-col" style="margin-right:0;" for="pref-perpage">Payment Method:</label><br/>
+                                            <select id="pref-perpage" class="form-control">
+                                                <option value="1">Cash</option>
+                                                <option value="2">Credit Card</option>
+                                                <option value="3">Checked</option>
+                                                <option value="4">Paypal</option>
+                                            </select>                                
+                                        </div> 
+                                        <div class="form-group" style="margin-right: 10px;">
+                                            <label class="filter-col" style="margin-right:0;" for="pref-search">Reference Number:</label><br/>
+                                            <input type="text" class="form-control" id="pref-search">
+                                        </div> 
+                                        <div class="form-group" style="margin-right: 10px;">
+                                            <label class="filter-col" style="margin-right:0;" for="pref-search">Which Line Item:</label><br/>
+                                            <input type="text" class="form-control" id="pref-search">
+                                        </div>
+
+                                        <div class="form-group" style="margin-right: 10px;">
+                                            <label class="filter-col" style="margin-right:0;" for="pref-search">Amount to be paid:</label><br/>
+                                            <input class="form-control" type='number' step='0.01' value='0.00' placeholder='0.00' />
+                                        </div>
+                                        <div class="form-group pull-right">
+                                            <label class="filter-col" style="margin-right:0;" for="pref-search">&nbsp;</label><br/>
+                                            <button type="submit" class="btn btn-primary filter-col " style="margin-bottom: 0px;">
+                                                <span class="glyphicon glyphicon-record"></span> Save
+                                            </button> 
+                                        </div>
+                                    </div>
+
+                                </form>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                <button type="button" class="btn btn-info" data-toggle="collapse" data-target="#filter-panel" style="margin-bottom: 10px;">
+                    <span class="glyphicon glyphicon-cog"></span> Filter/Search
+                </button>
+
+                     <div class="table-responsive">
+                        <table id="tbl_booking_lists" class="table table-striped table-bordered table-hover dataTables-example" >
+                            <thead>
+                                <tr>
+                                    <th style='text-align: center;'>#</th>
+                                    <th style='text-align: left;'>Magazine Name</th>
+                                    <th style='text-align: left;'># Issue</th>
+                                    <th style='text-align: left;'>Year Issue</th>
+                                    <th style='text-align: left;'>Sales Rep</th>
+                                    <th style='text-align: left;'>Client</th>
+                                    <th style='text-align: left;'>Agency</th>
+                                    <th style='text-align: center;'>Line Item</th>
+                                    <th style='text-align: center;'>Status / Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td style='text-align: center;'>1</td>
+                                    <td style='text-align: left;'>Sample</td>
+                                    <td style='text-align: center;'>Sample</td>
+                                    <td style='text-align: center;'>Sample</td>
+                                    <td style='text-align: left;'>Sample</td>
+                                    <td style='text-align: left;'>Sample</td>
+                                    <td style='text-align: left;'>Sample</td>
+                                    <td style='text-align: center;'>Sample</td>
+                                    <td style='text-align: left;'>Sample</td>
+                                </tr>
+                                <tr>
+                                    <td style='text-align: center;'>2</td>
+                                    <td style='text-align: left;'>Sample</td>
+                                    <td style='text-align: center;'>Sample</td>
+                                    <td style='text-align: center;'>Sample</td>
+                                    <td style='text-align: left;'>Sample</td>
+                                    <td style='text-align: left;'>Sample</td>
+                                    <td style='text-align: left;'>Sample</td>
+                                    <td style='text-align: center;'>Sample</td>
+                                    <td style='text-align: left;'>Sample</td>
+                                </tr>
                             </tbody>
-                    </table>
+                        </table>
+                    </div>
+
                 </div>
             </div>
         </div>
     </div>
 </div>
-
-</div>
 @endsection
 
-@section('scripts')
 
+@section('scripts')
 <script src="{{ asset('js/plugins/dataTables/datatables.min.js') }}"></script>
+<script src="{{ asset('js/plugins/datepicker/bootstrap-datepicker.js') }}"></script>
 <script>
     $(document).ready(function(){
         $('.dataTables-example').DataTable({
             dom: '<"html5buttons"B>lTfgitp',
             buttons: []
         });
+
+        $('#data_1 .input-group.date').datepicker({
+            todayBtn: "linked",
+            keyboardNavigation: false,
+            forceParse: false,
+            calendarWeeks: true,
+            autoclose: true
+        });
     });
+
+
 
     function open_preview(trans_number)
     {
